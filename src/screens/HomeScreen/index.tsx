2d30a3dc74dc373f78e08c71ca958d682ac7de9f
TAN CHONG HOW <chonghow@lavax.co>
2022-02-08 23:21:28 +0800

    chore: remove googleMapsApiKey from homeScreen

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,16 +1,11 @@
 import Map from "../../containers/Map";
 
-import { DEFAULT_API_KEY, DEFAULT_CENTER } from "../../config/constant";
+import { DEFAULT_CENTER } from "../../config/constant";
 import { containerStyle } from "./style";
 
 export const HomeScreen: React.FC = () => {
   return (
-    <Map
-      googleMapsApiKey={DEFAULT_API_KEY}
-      mapContainerStyle={containerStyle}
-      center={DEFAULT_CENTER}
-      zoom={12}
-    />
+    <Map mapContainerStyle={containerStyle} center={DEFAULT_CENTER} zoom={12} />
   );
 };
 
