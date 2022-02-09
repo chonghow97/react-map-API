62133b0bf6088382f16a717e76df8f1446833e0e
TAN CHONG HOW <chonghow@lavax.co>
2022-02-09 17:08:59 +0800

    feat: add Libraries type

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -1,6 +1,13 @@
 export type CoordinateType = google.maps.LatLng | google.maps.LatLngLiteral;
 export type PlaceResult = google.maps.places.PlaceResult;
 export type AutocompleteType = google.maps.places.Autocomplete;
+export type Libraries = (
+  | "drawing"
+  | "geometry"
+  | "localContext"
+  | "places"
+  | "visualization"
+)[];
 
 export type MapHookType = (
   setMapLists: React.Dispatch<
